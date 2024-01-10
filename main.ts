ed09e2dcb06c265a0a6fd4f33a059954199c8afa
PixelChip <89292951+Cuppy130@users.noreply.github.com>
2024-01-10 04:33:19 +0000

    run and jump created

--- main.ts
+++ main.ts
@@ -5,14 +5,25 @@ const ctx = getCanvas().getContext('2d');
 getCanvas().width = window.innerWidth;
 getCanvas().height = window.innerHeight;
 
-const keys = new keyHandler;
+function collision(a, b){return a.x <= b.x + b.w && a.x + a.w >= b.x && a.y <= b.y + b.h && a.y + a.h >= b.y}
+function innerCollision(a, b){
+    
+    a.x+=a.x/4;
+    a.y+=a.y/4;
+    a.w+=a.w/2;
+    a.h+=a.h/2;
+    return a.x <= b.x + b.w && a.x + a.w >= b.x && a.y <= b.y + b.h && a.y + a.h >= b.y
+}
+const screenOffset = {x: 5, y:20}
 
+const keys = new keyHandler;
+let scale = 20
 const audioSource = new Audio;
 audioSource.src = "/music/mainLevels/Dune Boi.mp3";
 audioSource.currentTime = 0 //offset
 
 class obj{
-    x=-1;
+    x=0;
     y=0;
     w=0;
     h=0;
@@ -32,26 +43,78 @@ class obj{
         this.x+=this.vx;
         this.y+=this.vy;
     }
-}
-
-class Player {
-    x = -1;
-    y = 0;
-    xv = .5 + 1/3;
-    yv = 0;
-    update(){
-
+    draw(ox, oy){
+        ctx.fillRect(this.x*scale+ox*scale-player.x*scale, this.y*scale+oy*scale, this.w*scale, this.h*scale)
     }
 }
 
-const player = new Player;
+class Player {
+    ox = 0;
+    oy = 0;
+    x = -1;
+    y = 0;
+    w = 1;
+    h = 1;
+    xv = 0;
+    yv = 0;
+    jforce = 0.025
+    gravity = 0.025;
+    isOnFloor = false;
+    orbPress = false;
+
+    top = this.y;
+    bottom = this.y + this.h;
+    left = this.x;
+    right = this.x + this.w;
+    constructor(x: number = 0, y: number = 0){
+        this.x = x;
+        this.y = y;
+        this.xv = 0.25
+    }
+    update(){
+        this.ox=this.x;
+        this.oy=this.y;
+
+        this.x+=this.xv;
+        this.y+=this.yv;
+        if(!this.isOnFloor){
+            this.yv+=this.gravity;
+        } else {
+            this.yv=0
+            if(keys.pressed("KeyW")){
+                this.yv = -.35;
+            }
+        }
+        
+    }
+    draw(ox, oy){
+        ctx.beginPath()
+        ctx.rect(ox*scale, this.y*scale+oy*scale, this.w*scale, this.h*scale)
+        ctx.stroke();
+    }
+}
+
+const player = new Player(-2, -2.5);
 const objects = [
-    new obj(2, 0, 1, 1),
-    new obj(4, 0, 1, 1)
+    new obj(0, 0, 20, 1),
 ]
 
-const loop = () => {
+function loop() {
+    ctx.clearRect(0, 0, 1920, 1080)
     player.update();
-    objects.forEach((obj) => {obj.update();obj.update()})
-    requestAnimationFrame(loop)
+    player.isOnFloor = false;
+    objects.forEach((obj) => {
+        obj.update();
+        obj.draw(screenOffset.x, screenOffset.y);
+        if(collision(player, obj)){
+            player.isOnFloor = true;
+            if(player.y+player.h > obj.y){
+                player.y = obj.y - player.h
+            }
+        }
+    })
+    player.draw(screenOffset.x, screenOffset.y);
+    requestAnimationFrame(loop);
 }
+
+loop();
